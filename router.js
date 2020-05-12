2b270f87856a105053ac0e9732a8fdddd2e12150
dmondo <dannysmondo@gmail.com>
2020-05-12 16:11:33 -0700

    feat(router): reverse proxy routing for each component

--- router.js
+++ router.js
@@ -4,13 +4,23 @@ const httpProxy = require('http-proxy');
 
 const apiProxy = httpProxy.createProxyServer();
 const reviewServer = 'http://localhost:1128';
+const shippingServer = 'http://localhost:9000';
+const carouselServer = 'http://localhost:3000';
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
 router.all('/rev/*', (req, res) => {
   apiProxy.web(req, res, { target: reviewServer });
+});
+
+router.all('/ship/*', (req, res) => {
+  apiProxy.web(req, res, { target: shippingServer });
+});
+
+router.all('/caro/*', (req, res) => {
+  apiProxy.web(req, res, { target: carouselServer });
 });
 
 module.exports = router;
