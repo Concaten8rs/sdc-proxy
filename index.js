7ec371f949f45df893189b0f2fce70b5dc8b39ce
dmondo <dannysmondo@gmail.com>
2020-05-11 18:19:33 -0700

    refactor(server): html served directly from root url

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
+const router = require('./router.js');
 
 const app = express();
-const apiProxy = httpProxy.createProxyServer();
-const reviewServer = 'http://localhost:1128';
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.all('/rev/*', (req, res) => {
-  apiProxy.web(req, res, { target: reviewServer });
-});
+app.use(router);
 
 const port = process.env.PORT || 1150;
 
--- router.js
+++ router.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
+const httpProxy = require('http-proxy');
+
+const apiProxy = httpProxy.createProxyServer();
+const reviewServer = 'http://localhost:1128';
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+router.all('/rev/*', (req, res) => {
+  apiProxy.web(req, res, { target: reviewServer });
 });
 
 module.exports = router;
