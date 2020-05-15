7de09f4d082ecb8ae003ac4c624cc986d1b9900a
dmondo <dannysmondo@gmail.com>
2020-05-14 18:17:14 -0700

    feat(docker): routing for docker

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const router = require('./router.js');
 
 const app = express();
 
 app.use(express.static('dist'));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 1150;
